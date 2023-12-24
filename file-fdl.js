528368285bfefc7a58df9e677b5af6f482ba7904
meksoftwaredev <146220688+meksoftwaredev@users.noreply.github.com>
2023-12-24 21:34:34 +0300

    Delete file-fdl.js

--- file-fdl.js
+++
@@ -1,8 +0,0 @@
-Notification.requestPermission()
-.then((permission) => {
- if (permission === 'granted') {
-    // Kullanıcı izin verdi, bildirim gönderebilirsiniz
- } else {
-    // Kullanıcı izin vermedi, bu yüzden bildirim gönderemezsiniz
- }
-});
